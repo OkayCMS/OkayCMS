fdc58a4d1791b03329878aed9f2f0cc8d63fcde4
OkayCMS <info@okay-cms.com>
2021-04-15 13:02:44 +0300

    do not import the old price from 1C if it is equal to or less than the main price

--- Okay/Modules/OkayCMS/Integration1C/Integration/Integration1C.php
+++ Okay/Modules/OkayCMS/Integration1C/Integration/Integration1C.php
@@ -34,6 +34,8 @@ class Integration1C
     public $exportPurchasesDiscountsSeparate = false;   // TRUE Экспортировать скидки товаров в заказе отдельно
     
     public $eraseComparePrice = false;   // TRUE сбрасывать старую цену, если она не пришла с 1С
+    
+    public $eraseComparePriceEqual = false;   // TRUE сбрасывать старую цену, если равна либо меньше основной цены
 
     public $guidPriceFrom1C = '';  // ID типа цены в 1С, который нужно загрузить в товар, если не указать, будет браться первая
 
