a406c324ca2bb268e4b66d687d73073b43ca17e8
Roman Fedotov <grey.matter.dp@gmail.com>
2021-04-26 11:22:39 +0300

    Editing modules translations

--- Okay/Entities/TranslationsEntity.php
+++ Okay/Entities/TranslationsEntity.php
@@ -6,24 +6,37 @@ namespace Okay\Entities;
 
 use Okay\Core\EntityFactory;
 use Okay\Core\Entity\Entity;
+use Okay\Core\Modules\Modules;
 use Okay\Core\TemplateConfig\FrontTemplateConfig;
 use Okay\Core\ServiceLocator;
 use Okay\Core\Modules\Extender\ExtenderFacade;
 
 class TranslationsEntity extends Entity
 {
+    const TRANS_T_THEME   = 'theme';
+    const TRANS_T_GENERAL = 'general';
+    const TRANS_T_MODULE  = 'module';
 
-    /**
-     * @var FrontTemplateConfig
-     */
+    /** @var FrontTemplateConfig */
     private $frontTemplateConfig;
 
-    /**
-     * @var LanguagesEntity
-     */
-    private $languages;
+    /** @var LanguagesEntity */
+    private $languagesEntity;
 
-    private $templateOnly = false;
+    /** @var Modules */
+    private $modules;
+
+
+    /** @var array general translations */
+    private $generalVars = [];
+
+    /** @var array theme translations */
+    private $themeVars = [];
+
+    /** @var array modules translations */
+    private $modulesVars = [];
+
+    /** @var array all translations */
     private $vars = [];
 
     public function __construct()
@@ -31,7 +44,9 @@ class TranslationsEntity extends Entity
         parent::__construct();
         $serviceLocator = ServiceLocator::getInstance();
         $this->frontTemplateConfig = $serviceLocator->getService(FrontTemplateConfig::class);
-        $this->languages      = $serviceLocator->getService(EntityFactory::class)->get(LanguagesEntity::class);
+        $this->modules             = $serviceLocator->getService(Modules::class);
+
+        $this->languagesEntity = $serviceLocator->getService(EntityFactory::class)->get(LanguagesEntity::class);
     }
     
     public function templateOnly($state)
@@ -50,11 +65,19 @@ class TranslationsEntity extends Entity
     {
         $translation = [];
         
-        foreach ($this->languages->find() as $l) {
-            $result = $this->initOneTranslation($l->label, $this->templateOnly);
-            if (isset($result[$id])) {
-                $translation['lang_' . $l->label] = $result[$id];
-                $translation['values'][$l->id] = $result[$id];
+        foreach ($this->languagesEntity->find() as $l) {
+            $modulesResult = $this->initOneModulesTranslation($l->label);
+            $themeResult   = $this->initOneThemeTranslation($l->label);
+            $generalResult = $this->initOneGeneralTranslation($l->label);
+            if (isset($modulesResult[$id])) {
+                $translation['lang_' . $l->label] = $modulesResult[$id];
+                $translation['values'][$l->id]    = $modulesResult[$id];
+            } else if (isset($themeResult[$id])) {
+                $translation['lang_' . $l->label] = $themeResult[$id];
+                $translation['values'][$l->id]    = $themeResult[$id];
+            } else if (isset($generalResult[$id])) {
+                $translation['lang_' . $l->label] = $generalResult[$id];
+                $translation['values'][$l->id]    = $generalResult[$id];
             }
         }
 
@@ -72,28 +95,20 @@ class TranslationsEntity extends Entity
 
     public function find(array $filter = [])
     {
-        $template_only = false;
         $force = false;
-        
-        if (isset($filter['template_only'])) {
-            $template_only = $filter['template_only'];
-        }
-        
-        if ($this->templateOnly === true) {
-            $template_only = true;
-        }
         
         if (isset($filter['force'])) {
             $force = $filter['force'];
         }
         
-        if (!empty($filter['lang_id']) && ($lang = $this->languages->get((int)$filter['lang_id']))) {
-            $result = $this->initOneTranslation($lang->label, $template_only, $force);
+        if (!empty($filter['lang_id']) && ($lang = $this->languagesEntity->get((int)$filter['lang_id']))) {
+            $result = $this->initOneTranslation($lang->label, $force);
         } elseif (!empty($filter['lang'])) {
-            $result = $this->initOneTranslation($filter['lang'], $template_only, $force);
+            $result = $this->initOneTranslation($filter['lang'], $force);
         } else {
-            die('get_translations empty(filter["lang"])');
+            $result = $this->initTranslations($force);
         }
+
         if (!empty($filter['sort'])) {
             switch ($filter['sort']) {
                 case 'label':
@@ -115,19 +130,51 @@ class TranslationsEntity extends Entity
         }
         $this->flush();
 
-        return ExtenderFacade::execute([static::class, __FUNCTION__], (object) $result, func_get_args());
+        return ExtenderFacade::execute([static::class, __FUNCTION__], $result, func_get_args());
     }
 
     public function update($id, $data)
     {
         $data = (array)$data;
         $this->initTranslations(true);
-        foreach ($this->vars as $langLabel=>&$translations) {
-            if ($id != $data['label']) {
-                unset($translations[$id]);
+        foreach ($this->languagesEntity->find() as $l) {
+            if (isset($this->modulesVars[$l->label][$id])) {
+                $translation = $this->vars[$l->label][$id];
+                if ($id != $data['label']) {
+                    unset($this->modulesVars[$l->label][$id]);
+                    unset($this->vars[$l->label][$id]);
+                }
+
+                $translation->value = $data['lang_'.$l->label];
+                $this->modulesVars[$l->label][$data['label']] = $translation;
+                $this->vars[$l->label][$data['label']] = $translation;
+                $this->writeModuleTranslation(
+                    $l->label,
+                    $id,
+                    $data['label'],
+                    $translation->value,
+                    $translation->module->vendor,
+                    $translation->module->name
+                );
+
+                if (isset($this->themeVars[$l->label][$id]) && $id != $data['label']) {
+                    $translation = $this->themeVars[$l->label][$id];
+                    unset($this->themeVars[$l->label][$id]);
+                    $this->themeVars[$l->label][$data['label']] = $translation;
+                    $this->writeThemeTranslations($l->label, $this->themeVars[$l->label]);
+                }
+            } else {
+                if (isset($this->themeVars[$l->label][$id]) && $id != $data['label']) {
+                    unset($this->themeVars[$l->label][$id]);
+                }
+                $translation = (object) [
+                    'value' => $data['lang_'.$l->label],
+                    'type' => self::TRANS_T_THEME
+                ];
+                $this->vars[$l->label][$data['label']]      = $translation;
+                $this->themeVars[$l->label][$data['label']] = $translation;
+                $this->writeThemeTranslations($l->label, $this->themeVars[$l->label]);
             }
-            $translations[$data['label']] = $data['lang_'.$langLabel];
-            $this->writeTranslations($langLabel, $translations);
         }
 
         return ExtenderFacade::execute([static::class, __FUNCTION__], $data['label'], func_get_args());
@@ -139,13 +186,17 @@ class TranslationsEntity extends Entity
         if (empty($ids)) {
             return ExtenderFacade::execute([static::class, __FUNCTION__], null, func_get_args());
         }
-
         $this->initTranslations(true);
-        foreach ($this->vars as $langLabel=>&$translations) {
+
+        foreach ($this->languagesEntity->find() as $l) {
             foreach ($ids as $id) {
-                unset($translations[$id]);
+                unset($this->themeVars[$l->label][$id]);
+
+                if (!isset($this->modulesVars[$l->label][$id])) {
+                    unset($this->vars[$l->label][$id]);
+                }
             }
-            $this->writeTranslations($langLabel, $translations);
+            $this->writeThemeTranslations($l->label, $this->themeVars[$l->label]);
         }
 
         return ExtenderFacade::execute([static::class, __FUNCTION__], null, func_get_args());
@@ -211,28 +262,93 @@ class TranslationsEntity extends Entity
         return ExtenderFacade::execute([static::class, __FUNCTION__], null, func_get_args());
     }
 
-    private function initTranslations($template_only = false) 
+    public function initTranslations($force = false)
     {
-        foreach ($this->languages->find() as $l) {
-            $this->initOneTranslation($l->label, $template_only);
+        foreach ($this->languagesEntity->find() as $l) {
+            $this->initOneTranslation($l->label, $force);
         }
         return ExtenderFacade::execute([static::class, __FUNCTION__], $this->vars, func_get_args());
     }
 
-    private function initOneTranslation($label = "", $template_only = false, $force = false) 
+    public function initOneTranslation($langLabel, $force = false)
     {
-        if (empty($label)) {
+        if (empty($langLabel)) {
             return false;
         }
 
         if ($force === true) {
-            unset($this->vars[$label]);
+            unset($this->vars[$langLabel]);
+        }
+
+        if (!isset($this->vars[$langLabel])) {
+            $translations = [];
+            if ($generalVars = $this->initOneGeneralTranslation($langLabel, $force)) {
+                foreach ($generalVars as $id => $generalVar) {
+                    $translations[$id] = $generalVar;
+                }
+            }
+            if ($themeVars = $this->initOneThemeTranslation($langLabel, $force)) {
+                foreach ($themeVars as $id => $themeVar) {
+                    $translations[$id] = $themeVar;
+                }
+            }
+            if ($modulesVars = $this->initOneModulesTranslation($langLabel, $force)) {
+                foreach ($modulesVars as $id => $moduleVar) {
+                    $translations[$id] = $moduleVar;
+                }
+            }
+            $this->vars[$langLabel] = ExtenderFacade::execute([static::class, __FUNCTION__], $translations, func_get_args());
+        }
+
+        return $this->vars[$langLabel];// no ExtenderFacade
+    }
+
+    public function initOneGeneralTranslation($langLabel, $force = false)
+    {
+        if (empty($langLabel)) {
+            return false;
+        }
+
+        if ($force === true) {
+            unset($this->generalVars[$langLabel]);
+        }
+
+        if (!isset($this->generalVars[$langLabel])) {
+            $translations = [];
+            $fileLangGeneral = __DIR__ . '/../lang_general/' . $langLabel . '.php';
+            if (file_exists($fileLangGeneral)) {
+                $lang_general = [];
+                if ($force === false) {
+                    require_once $fileLangGeneral;
+                } else {
+                    require $fileLangGeneral;
+                }
+
+                foreach ($lang_general as $id => $translationValue) {
+                    $translations[$id] = (object) [
+                        'value' => $translationValue,
+                        'type'  => self::TRANS_T_GENERAL
+                    ];
+                }
+            }
+            $this->generalVars[$langLabel] = ExtenderFacade::execute([static::class, __FUNCTION__], $translations, func_get_args());
+        }
+        return $this->generalVars[$langLabel];// no ExtenderFacade
+    }
+
+    public function initOneThemeTranslation($langLabel, $force = false)
+    {
+        if (empty($langLabel)) {
+            return false;
+        }
+
+        if ($force === true) {
+            unset($this->themeVars[$langLabel]);
         }
         
-        if (!isset($this->vars[$label])) {
-            
-            $langFile = $this->getReadLangFile($label, $this->frontTemplateConfig->getTheme());
-            
+        if (!isset($this->themeVars[$langLabel])) {
+            $translations = [];
+            $langFile = $this->getReadLangFile($langLabel, $this->frontTemplateConfig->getTheme());
             if (file_exists($langFile)) {
                 $lang = array();
                 if ($force === false) {
@@ -240,50 +356,104 @@ class TranslationsEntity extends Entity
                 } else {
                     require $langFile;
                 }
-                
-                // Подключаем файл переводов по умолчанию, но с возможностью переопределить в самом шаблоне
-                if ($template_only === false) {
-                    $fileLangGeneral = __DIR__ . '/../lang_general/' . $label . '.php';
-                    if (file_exists($fileLangGeneral)) {
-                        $lang_general = array();
-                        if ($force === false) {
-                            require_once $fileLangGeneral;
-                        } else {
-                            require $fileLangGeneral;
-                        }
-                        $lang = $lang + $lang_general;
-                    }
+
+                foreach ($lang as $id => $translationValue) {
+                    $translations[$id] = (object)[
+                        'value' => $translationValue,
+                        'type' => self::TRANS_T_THEME
+                    ];
                 }
-
-                $this->vars[$label] = ExtenderFacade::execute([static::class, __FUNCTION__], $lang, func_get_args());
-            } else {
-                $this->vars[$label] = ExtenderFacade::execute([static::class, __FUNCTION__], [], func_get_args());
             }
+            $this->themeVars[$langLabel] = ExtenderFacade::execute([static::class, __FUNCTION__], $translations, func_get_args());
         }
-
-        return $this->vars[$label];// no ExtenderFacade
+        return $this->themeVars[$langLabel];// no ExtenderFacade
     }
 
-    private function writeTranslations($langLabel, $translations)
+    public function initOneModulesTranslation($langLabel, $force = false)
+    {
+        if (empty($langLabel)) {
+            return false;
+        }
+
+        if ($force === true) {
+            unset($this->modulesVars[$langLabel]);
+        }
+
+        if (!isset($this->modulesVars[$langLabel])) {
+            $translations = [];
+            foreach ($this->modules->getRunningModules() as $runningModule) {
+                $moduleTranslations = $this->modules->getModuleFrontTranslations($runningModule['vendor'], $runningModule['module_name'], $langLabel);
+                foreach ($moduleTranslations as $id => $translation) {
+                    $translations[$id] = (object) [
+                        'value'  => $translation,
+                        'type'   => self::TRANS_T_MODULE,
+                        'module' => (object) [
+                            'vendor' => $runningModule['vendor'],
+                            'name'   => $runningModule['module_name']
+                        ]
+                    ];
+                }
+            }
+            $this->modulesVars[$langLabel] = ExtenderFacade::execute([static::class, __FUNCTION__], $translations, func_get_args());
+        }
+        return $this->modulesVars[$langLabel];// no ExtenderFacade
+    }
+
+    private function writeThemeTranslations($langLabel, $translations)
     {
         if (empty($langLabel)) {
             return false;
         }
 
         $langFile = $this->getWriteLangFile($langLabel, $this->frontTemplateConfig->getTheme());
-        
+
+        $translationsToWrite = [];
+        foreach ($translations as $id => $translation) {
+            $translationsToWrite[$id] = $translation->value;
+        }
+
+        $this->writeTranslationsToLangFile($langFile, $translationsToWrite);
+            
+        return ExtenderFacade::execute([static::class, __FUNCTION__], null, func_get_args());
+    }
+
+    private function writeModuleTranslation(
+        $langLabel,
+        $oldId,
+        $newId,
+        $translation,
+        $vendor,
+        $name
+    ) {
+        if (empty($langLabel)) {
+            return false;
+        }
+
+        $translations = $this->modules->getModuleFrontTranslations($vendor, $name, $langLabel);
+        if ($oldId != $newId) {
+            unset($translations[$oldId]);
+        }
+        $translations[$newId] = $translation;
+
+        $this->writeTranslationsToLangFile($this->getWriteModuleLangFile($langLabel, $vendor, $name), $translations);
+
+        return ExtenderFacade::execute([static::class, __FUNCTION__], null, func_get_args());
+    }
+
+    public function writeTranslationsToLangFile($langFile, $translations)
+    {
         $content = "<?php\n\n";
         $content .= "\$lang = [];\n";
-        foreach($translations as $label=>$value) {
-            $content .= "\$lang['".$label."'] = '".addcslashes($value, "\n\r\\\"'")."';\n";
+        foreach($translations as $label => $translation) {
+            $content .= "\$lang['".$label."'] = '".addcslashes($translation, "\n\r\\\"'")."';\n";
         }
         $file = fopen($langFile, 'w');
         fwrite($file, $content);
         fclose($file);
-            
+
         return ExtenderFacade::execute([static::class, __FUNCTION__], null, func_get_args());
     }
-    
+
     private function getReadLangFile($langLabel, $theme)
     {
         $langFile = __DIR__ . '/../../design/' . $theme . '/lang/' . $langLabel . '.php';
@@ -293,6 +463,12 @@ class TranslationsEntity extends Entity
     private function getWriteLangFile($langLabel, $theme)
     {
         $langFile = __DIR__ . '/../../design/' . $theme . '/lang/' . $langLabel . '.php';
+        return ExtenderFacade::execute([static::class, __FUNCTION__], $langFile, func_get_args());
+    }
+
+    private function getWriteModuleLangFile($langLabel, $vendor, $name)
+    {
+        $langFile = $this->modules->getModuleFrontTranslationsLangFile($vendor, $name, $langLabel);
         return ExtenderFacade::execute([static::class, __FUNCTION__], $langFile, func_get_args());
     }
 }
