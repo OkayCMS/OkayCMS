f5c63a5f558b715c3c2b4c31eb508261464d78e5
DeveloperNiko <nikita.voronov.work@gmail.com>
2022-03-10 14:38:25 +0200

    update sql file 4.3.0

--- 1DB_changes/update_4.3.0.sql
+++ 1DB_changes/update_4.3.0.sql
@@ -1,3 +1,4 @@
+UPDATE `ok_menu_items` SET `url` = 'all-products/filter-discounted' WHERE `url` LIKE '%discounted%';
 DELETE FROM ok_lang_pages WHERE page_id IN (SELECT id FROM ok_pages WHERE url IN ('bestsellers', 'discounted'));
 DELETE FROM ok_pages WHERE url IN ('bestsellers', 'discounted');
 
