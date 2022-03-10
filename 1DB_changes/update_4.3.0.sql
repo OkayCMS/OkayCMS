18a52614e3486fc441fa2d81d28e7e5a1d9adc93
DeveloperNiko <nikita.voronov.work@gmail.com>
2022-03-10 14:07:31 +0200

    update sql file 4.3.0

--- 1DB_changes/update_4.3.0.sql
+++ 1DB_changes/update_4.3.0.sql
@@ -1,7 +1,4 @@
 DELETE FROM ok_lang_pages WHERE page_id IN (SELECT id FROM ok_pages WHERE url IN ('bestsellers', 'discounted'));
 DELETE FROM ok_pages WHERE url IN ('bestsellers', 'discounted');
-DELETE FROM ok_currencies WHERE code = 'RUB';
-DELETE FROM ok_lang_currencies WHERE sign = 'руб';
-UPDATE ok_currencies t SET t.position = 1 WHERE t.code = 'UAH';
 
 INSERT INTO ok_settings (param, value) VALUES('features_max_count_products', 10);
