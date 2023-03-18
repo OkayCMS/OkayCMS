51ae2f570c3b61cefd7ddbac1e695bc67037c44f
Vitalis11 <vbelyaev2005@gmail.com>
2023-03-18 19:54:00 +0200

    Core/Response: type hints, switch/case to array, chaining

--- Okay/Core/Response.php
+++ Okay/Core/Response.php
@@ -8,7 +8,7 @@ use Okay\Core\Adapters\Response\AdapterManager;
 
 class Response
 {
-
+    
     private $content = [];
     private $adapterManager;
     private $headers;
@@ -22,43 +22,48 @@ class Response
         $this->type = RESPONSE_HTML;
         $this->addHeader('X-Powered-CMS: OkayCMS ' . $version);
     }
-
+    
     /**
-     * Метод перенаправления на другой ресурс. При помощи функции exit() 
+     * Метод перенаправления на другой ресурс. При помощи функции exit()
      * прекращает исполнение кода расположенного ниже вызова метода.
-     * 
-     * @param $resource
-     * @param $responseCode
-     * 
+     *
+     * @param string $resource
+     * @param int $responseCode
+     *
      * @return void
      * @throws \Exception
      */
-    public static function redirectTo($resource, $responseCode = 302)
+    public static function redirectTo(string $resource, int $responseCode = 302): void
     {
-        $responseCode = (int) $responseCode;
-
-        if (!in_array($responseCode, [301, 302, 307, 308])) {
-            throw new \Exception("$responseCode is not valid redirect response code.");
+        if (!in_array($responseCode, [301, 302, 303, 307, 308])) {
+            throw new \Exception("$responseCode is not a valid redirect response code.");
         }
-
-        $headerContent = 'location: '.$resource;
+        
+        $headerContent = 'Location: ' . $resource;
         header($headerContent, false, $responseCode);
-        exit();
+        exit;
     }
-
-    public function setStatusCode($statusCode)
+    
+    public function setStatusCode($statusCode): self
     {
+        if (empty($statusCode)) {
+            throw new \Exception('Response status code cannot be empty');
+        } elseif (in_array($statusCode, [301, 302, 303, 307, 308])) {
+            throw new \Exception("Please use Okay\\Core\\Response::redirectTo method for $statusCode redirect");
+        } elseif (empty(self::status_codes_messages[$statusCode])) {
+            throw new \Exception("Response status code $statusCode is invalid or not supported");
+        }
         $this->statusCode = $statusCode;
         return $this;
     }
-
-    public function addHeader($headerContent, $replace = true, $responseCode = null)
+    
+    public function addHeader($headerContent, $replace = true, $responseCode = null): self
     {
         $this->headers[] = [$headerContent, $replace, $responseCode];
         return $this;
     }
     
-    public function setContent($content, $type = null)
+    public function setContent($content, $type = null): self
     {
         if ($type !== null) {
             $this->type = trim($type);
@@ -67,35 +72,36 @@ class Response
         return $this;
     }
     
-    public function setContentType($type)
+    public function setContentType(string $type): self
     {
         $this->type = trim($type);
+        return $this;
     }
     
-    public function getContentType()
+    public function getContentType(): string
     {
         return $this->type;
     }
     
-    public function getContent()
+    public function getContent(): array
     {
         return $this->content;
     }
-
+    
     /**
-     * В отличии от метода sendContent(), этот метод непосредственно сейчас отправляет данные.
+     * В отличие от метода sendContent(), этот метод непосредственно сейчас отправляет данные.
      * Нужно обязательно до первого его вызова установить тип данных setContent(), и вызвать sendHeaders()
-     * 
+     *
      * Пример:
      * $response->setContentType(RESPONSE_XML);
      * $response->sendHeaders();
      * $response->sendStream('<xml>');
      * $response->sendStream('</xml>');
-     * 
+     *
      * @param string $content
-     * @param null $type
+     * @param string|null $type
      */
-    public function sendStream($content, $type = null)
+    public function sendStream(string $content, ?string $type): void
     {
         $this->isStream = true;
         
@@ -105,201 +111,148 @@ class Response
         
         /** @var Adapters\Response\AbstractResponse $adapter */
         $adapter = $this->adapterManager->getAdapter($this->type);
-
+        
         $adapter->send([$content]);
     }
     
-    public function sendContent()
+    public function sendContent(): self
     {
         if ($this->isStream === true) {
-            return;
+            return $this;
         }
         
         /** @var Adapters\Response\AbstractResponse $adapter */
         $adapter = $this->adapterManager->getAdapter($this->type);
-
+        
         $this->sendHeaders();
         
         $adapter->send($this->content);
+        
+        return $this;
     }
-
-    public function sendHeaders()
+    
+    public function sendHeaders(): self
     {
         $this->commitStatusCode();
-
+        
         /** @var Adapters\Response\AbstractResponse $adapter */
         $adapter = $this->adapterManager->getAdapter($this->type);
-
+        
         // Добавляем специальные заголовки, от драйвера
         foreach ($adapter->getSpecialHeaders() as $header) {
-            $this->addHeader($header, true);
+            $this->addHeader($header);
         }
-
-        if ($this->headersExists()) {
+        
+        if (!empty($this->headers)) {
             foreach ($this->headers as $k => $header) {
                 list($headerContent, $replace, $responseCode) = $header;
-
+                
                 if (is_null($responseCode)) {
                     header($headerContent, $replace);
                     continue;
                 }
-
+                
                 header($headerContent, $replace, $responseCode);
                 unset($this->headers[$k]);
             }
         }
+        return $this;
     }
-
-    private function headersExists()
+    
+    public function setHeaderLastModify(string $lastModify): self
     {
-        if (!empty($this->headers)) {
-            return true;
-        }
-
-        return false;
-    }
-
-    public function setHeaderLastModify($lastModify)
-    {
-        $lastModify = empty($lastModify) ? date("Y-m-d H:i:s") : $lastModify;
-        $tmpDate = date_parse($lastModify);
-        @$lastModifiedUnix = mktime( $tmpDate['hour'], $tmpDate['minute'], $tmpDate['second '], $tmpDate['month'],$tmpDate['day'],$tmpDate['year'] );
-
-        //Проверка модификации страницы
+        $lastModifiedUnix = $lastModify ? strtotime($lastModify) : time();
+        
+        // Проверка модификации страницы
         $lastModified = gmdate("D, d M Y H:i:s \G\M\T", $lastModifiedUnix);
         $ifModifiedSince = false;
-
+        
         if (isset($_ENV['HTTP_IF_MODIFIED_SINCE'])) {
             $ifModifiedSince = strtotime(substr($_ENV['HTTP_IF_MODIFIED_SINCE'], 5));
         }
-
+        
         if (isset($_SERVER['HTTP_IF_MODIFIED_SINCE'])) {
             $ifModifiedSince = strtotime(substr($_SERVER['HTTP_IF_MODIFIED_SINCE'], 5));
         }
-
+        
         if ($ifModifiedSince && $ifModifiedSince >= $lastModifiedUnix) {
             $this->setStatusCode(304)->sendHeaders();
             exit;
         }
-
-        $this->addHeader('Last-Modified: '. $lastModified);
+        
+        $this->addHeader('Last-Modified: ' . $lastModified);
+        
+        return $this;
     }
-
-    private function commitStatusCode()
+    
+    private function commitStatusCode(): void
     {
-        if (empty($this->statusCode)) {
-            throw new \Exception('Response status code cannot be empty');
-        }
-
-        switch ($this->statusCode) {
-            // 5XX
-            case 521:
-                $this->addHeader($_SERVER['SERVER_PROTOCOL'].' 521 Web Server Is Down');
-                break;
-            case 520:
-                $this->addHeader($_SERVER['SERVER_PROTOCOL'].' 520 Unknown Error');
-                break;
-            case 511:
-                $this->addHeader($_SERVER['SERVER_PROTOCOL'].' 511 Network Authentication Required');
-                break;
-            case 510:
-                $this->addHeader($_SERVER['SERVER_PROTOCOL'].' 510 Not Extended');
-                break;
-            case 509:
-                $this->addHeader($_SERVER['SERVER_PROTOCOL'].' 509 Bandwidth Limit Exceeded');
-                break;
-            case 508:
-                $this->addHeader($_SERVER['SERVER_PROTOCOL'].' 508 Loop Detected');
-                break;
-            case 507:
-                $this->addHeader($_SERVER['SERVER_PROTOCOL'].' 507 Insufficient Storage');
-                break;
-            case 506:
-                $this->addHeader($_SERVER['SERVER_PROTOCOL'].' 506 Variant Also Negotiates');
-                break;
-            case 505:
-                $this->addHeader($_SERVER['SERVER_PROTOCOL'].' 505 HTTP Version Not Supported');
-                break;
-            case 504:
-                $this->addHeader($_SERVER['SERVER_PROTOCOL'].' 504 Gateway Timeout');
-                break;
-            case 503:
-                $this->addHeader($_SERVER['SERVER_PROTOCOL'].' 503 Service Unavailable');
-                break;
-            case 502:
-                $this->addHeader($_SERVER['SERVER_PROTOCOL'].' 502 Bad Gateway');
-                break;
-            case 501:
-                $this->addHeader($_SERVER['SERVER_PROTOCOL'].' 501 Not Implemented');
-                break;
-            case 500:
-                $this->addHeader($_SERVER['SERVER_PROTOCOL'].' 500 Internal Server Error');
-                break;
-
-            // 4XX
-            case 423:
-                $this->addHeader($_SERVER['SERVER_PROTOCOL'].' 423 Locked');
-                break;
-            case 417:
-                $this->addHeader($_SERVER['SERVER_PROTOCOL'].' 417 Expectation Failed');
-                break;
-            case 410:
-                $this->addHeader($_SERVER['SERVER_PROTOCOL'].' 410 Gone');
-                break;
-            case 405:
-                $this->addHeader($_SERVER['SERVER_PROTOCOL'].' 405 Method Not Allowed');
-                break;
-            case 404:
-                $this->addHeader($_SERVER['SERVER_PROTOCOL'].' 404 Not Found');
-                break;
-            case 401:
-                $this->addHeader($_SERVER['SERVER_PROTOCOL'].' 401 Unauthorized');
-                break;
-            case 400:
-                $this->addHeader($_SERVER['SERVER_PROTOCOL'].' 400 Bad Request');
-                break;
-
-            // 3XX
-            case 308:
-                throw new \Exception('Please use Okay\Core\Response::redirectTo method for 308 redirect');
-                break;
-            case 307:
-                throw new \Exception('Please use Okay\Core\Response::redirectTo method for 307 redirect');
-                break;
-            case 304:
-                $this->addHeader($_SERVER['SERVER_PROTOCOL'].' 304 Not Modified');
-                break;
-            case 302:
-                throw new \Exception('Please use Okay\Core\Response::redirectTo method for 302 redirect');
-                break;
-            case 301:
-                throw new \Exception('Please use Okay\Core\Response::redirectTo method for 301 redirect');
-                break;
-
-            // 2XX
-            case 206:
-                $this->addHeader($_SERVER['SERVER_PROTOCOL'].' 206 Partial Content');
-                break;
-            case 205:
-                $this->addHeader($_SERVER['SERVER_PROTOCOL'].' 205 Reset Content');
-                break;
-            case 200:
-                $this->addHeader($_SERVER['SERVER_PROTOCOL'].' 200 OK');
-                break;
-
-            // 1XX
-            case 102:
-                $this->addHeader($_SERVER['SERVER_PROTOCOL'].' 100 Continue');
-                break;
-            case 101:
-                $this->addHeader($_SERVER['SERVER_PROTOCOL'].' 101 Switching Protocols');
-                break;
-            case 100:
-                $this->addHeader($_SERVER['SERVER_PROTOCOL'].' 102 Processing');
-                break;
-
-            default:
-                throw new \Exception('Response status code "'.$this->statusCode.'" incorrect or don`t supported');
-        }
+        $this->addHeader(sprintf('%s %d %s',
+            $_SERVER['SERVER_PROTOCOL'],
+            $this->statusCode,
+            self::status_codes_messages[$this->statusCode]
+        ));
     }
+    
+    const status_codes_messages = [
+        100 => 'Continue',
+        101 => 'Switching Protocols',
+        102 => 'Processing',
+        
+        200 => 'OK',
+        201 => 'Created',
+        202 => 'Accepted',
+        203 => 'Non-Authoritative Information',
+        204 => 'No Content',
+        205 => 'Reset Content',
+        206 => 'Partial Content',
+        207 => 'Multi-Status',
+        208 => 'Already Reported',
+        
+        304 => 'Not Modified',
+        
+        400 => 'Bad Request',
+        401 => 'Unauthorized',
+        402 => 'Payment Required',
+        403 => 'Forbidden',
+        404 => 'Not Found',
+        405 => 'Method Not Allowed',
+        406 => 'Not Acceptable',
+        407 => 'Proxy Authentication Required',
+        408 => 'Request Timeout',
+        409 => 'Conflict',
+        410 => 'Gone',
+        411 => 'Length Required',
+        412 => 'Precondition Failed',
+        413 => 'Content Too Large',
+        414 => 'URI Too Long',
+        415 => 'Unsupported Media Type',
+        416 => 'Range Not Satisfiable',
+        417 => 'Expectation Failed',
+        421 => 'Misdirected Request',
+        422 => 'Unprocessable Content',
+        423 => 'Locked',
+        424 => 'Failed Dependency',
+        425 => 'Too Early',
+        426 => 'Upgrade Required',
+        428 => 'Precondition Required',
+        429 => 'Too Many Requests',
+        431 => 'Request Header Fields Too Large',
+        451 => 'Unavailable For Legal Reasons',
+        
+        500 => 'Internal Server Error',
+        501 => 'Not Implemented',
+        502 => 'Bad Gateway',
+        503 => 'Service Unavailable',
+        504 => 'Gateway Timeout',
+        505 => 'HTTP Version Not Supported',
+        506 => 'Variant Also Negotiates',
+        507 => 'Insufficient Storage',
+        508 => 'Loop Detected',
+        509 => 'Bandwidth Limit Exceeded',
+        510 => 'Not Extended',
+        511 => 'Network Authentication Required',
+        520 => 'Unknown Error',
+        521 => 'Web Server Is Down',
+    ];
 }
