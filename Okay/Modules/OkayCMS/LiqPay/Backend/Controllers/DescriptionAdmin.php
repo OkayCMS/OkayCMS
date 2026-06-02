44129e132b98647666869b353ed43acfcef3680c
DeveloperM <max33maxn@gmail.com>
2026-06-02 20:16:09 +0300

    Modifying the module to send information about products to the service for fiscalization

--- Okay/Modules/OkayCMS/LiqPay/Backend/Controllers/DescriptionAdmin.php
+++ Okay/Modules/OkayCMS/LiqPay/Backend/Controllers/DescriptionAdmin.php
@@ -10,6 +10,13 @@ class DescriptionAdmin extends IndexAdmin
 {
     public function fetch()
     {
+        if ($this->request->method('POST')) {
+            $this->settings->set('liq_pay_pay_send_receipts_client_after_fiscalization', $this->request->post('liq_pay_pay_send_receipts_client_after_fiscalization', 'integer', 0));
+            $this->settings->set('liq_pay_pay_send_receipts_admin_after_fiscalization', $this->request->post('liq_pay_pay_send_receipts_admin_after_fiscalization', 'integer', 0));
+
+            $this->design->assign('message_success', 'saved');
+        }
+
         $this->response->setContent($this->design->fetch('description.tpl'));
     }
 }
--- Okay/Modules/OkayCMS/LiqPay/PaymentForm.php
+++ Okay/Modules/OkayCMS/LiqPay/PaymentForm.php
@@ -1,29 +1,48 @@
 <?php
 
+/* https://www.liqpay.ua/uk/doc/api/internet_acquiring/checkout?tab=1 */
 
 namespace Okay\Modules\OkayCMS\LiqPay;
 
 
 use Okay\Core\EntityFactory;
+use Okay\Core\FrontTranslations;
 use Okay\Core\Modules\AbstractModule;
 use Okay\Core\Modules\Interfaces\PaymentFormInterface;
 use Okay\Core\Money;
 use Okay\Core\Router;
+use Okay\Core\Settings;
 use Okay\Entities\CurrenciesEntity;
 use Okay\Entities\OrdersEntity;
 use Okay\Entities\PaymentsEntity;
+use Okay\Entities\PurchasesEntity;
+use Okay\Entities\VariantsEntity;
 
 class PaymentForm extends AbstractModule implements PaymentFormInterface
 {
-
+    /* EntityFactory $entityFactory */
     private $entityFactory;
+
+    /* Money $money */
     private $money;
 
-    public function __construct(EntityFactory $entityFactory, Money $money)
-    {
+    /* FrontTranslations $frontTranslations */
+    private $frontTranslations;
+
+    /* Settings $settings */
+    private $settings;
+
+    public function __construct(
+        EntityFactory     $entityFactory,
+        Money             $money,
+        FrontTranslations $frontTranslations,
+        Settings $settings
+    ){
         parent::__construct();
-        $this->entityFactory = $entityFactory;
-        $this->money = $money;
+        $this->entityFactory     = $entityFactory;
+        $this->money             = $money;
+        $this->frontTranslations = $frontTranslations;
+        $this->settings          = $settings;
     }
 
     /**
@@ -40,6 +59,9 @@ class PaymentForm extends AbstractModule implements PaymentFormInterface
         /** @var CurrenciesEntity $currenciesEntity */
         $currenciesEntity = $this->entityFactory->get(CurrenciesEntity::class);
 
+        /** @var PurchasesEntity $purchasesEntity */
+        $purchasesEntity = $this->entityFactory->get(PurchasesEntity::class);
+
         $order = $ordersEntity->get((int)$orderId);
         $liqPayOrderId = $order->id . "-" . rand(100000, 999999);
         $paymentMethod = $paymentsEntity->get($order->payment_method_id);
@@ -48,16 +70,57 @@ class PaymentForm extends AbstractModule implements PaymentFormInterface
 
         $price = $this->money->convert($order->total_price, $paymentMethod->currency_id, false, false, 2);
 
-        // описание заказа
-        // order description
-        $desc = 'Оплата заказа №'.$order->id;
+        // опис замовлення
+        $desc = $this->frontTranslations->getTranslation('okaycms__liq_pay__order_description') . ' №'.$order->id;
 
         $resultUrl = Router::generateUrl('order', ['url' => $order->url], true);
         $serverUrl = Router::generateUrl('OkayCMS_LiqPay_callback', [], true);
 
-        $privateKey = $settings['liq_pay_private_key'];
-        $publicKey = $settings['liq_pay_public_key'];
+        $privateKey  = $settings['liq_pay_private_key'];
+        $publicKey   = $settings['liq_pay_public_key'];
         $paymentType = $settings['pay_types'];
+
+        //  дані для фіскалізації
+        $purchases = $purchasesEntity->find(['order_id' => $order->id]);
+
+        $items = [];
+
+        $taxs [] = [ "name"=> "Без ПДВ 0%", "letter"=> "А", "prc"=> 0, "type"=> 0];
+
+        if (!empty($purchases)) {
+            foreach ($purchases as $purchase) {
+                $amount     = (int) $purchase->amount;
+                $itemPrice  = round($this->money->convert((float) $purchase->price, $paymentMethod->currency_id, false, false, 2), 2);
+                $cost       = round($this->money->convert(($itemPrice * $amount), $paymentMethod->currency_id, false, false, 2), 2);
+
+                $items[] = [
+                    "amount"        => $amount,
+                    "price"         => $itemPrice,
+                    "cost"          => $cost,
+                    "categoryname"  => "Дитячий одяг",
+                    "name"          => $purchase->product_name,
+                    "unitname"      => "Штука",
+                    "vndcode"       => $purchase->sku,
+                    "taxs"          => $taxs
+                ];
+            };
+        };
+
+        $rro_info['items'] = $items;
+
+        if (!empty($orderEmailAdmin = $this->settings->get('order_email'))
+            && !empty($sendAdmin = $this->settings->get('liq_pay_pay_send_receipts_admin_after_fiscalization'))
+        ){
+            $deliveryEmails[] = $orderEmailAdmin;
+        }
+        if (!empty($order->email)
+            && !empty($sendRecepient = $this->settings->get('liq_pay_pay_send_receipts_client_after_fiscalization'))
+        ){
+            $deliveryEmails[] = $order->email;
+        }
+        if (!empty($deliveryEmails)){
+            $rro_info['delivery_emails'] = array_values($deliveryEmails);
+        }
 
         $data_array = [
             'version'      => 3,
@@ -70,6 +133,7 @@ class PaymentForm extends AbstractModule implements PaymentFormInterface
             'order_id'     => $liqPayOrderId,
             'result_url'   => $resultUrl,
             'server_url'   => $serverUrl,
+            'rro_info'     => $rro_info,
         ];
 
         if(!empty($paymentType) && $paymentType !== 'default') {
