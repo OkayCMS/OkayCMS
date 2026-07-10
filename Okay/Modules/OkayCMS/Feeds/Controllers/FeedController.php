9fb3dda70b791bd68757021d5557b2d738aba5c6
DeveloperM <max33maxn@gmail.com>
2026-07-10 14:23:33 +0300

    Do not display feeds in languages ​​that are disabled

--- Okay/Modules/OkayCMS/Feeds/Controllers/FeedController.php
+++ Okay/Modules/OkayCMS/Feeds/Controllers/FeedController.php
@@ -3,6 +3,8 @@
 namespace Okay\Modules\OkayCMS\Feeds\Controllers;
 
 use Okay\Controllers\AbstractController;
+use Okay\Core\Languages;
+use Okay\Entities\LanguagesEntity;
 use Okay\Modules\OkayCMS\Feeds\Entities\FeedsEntity;
 use Okay\Modules\OkayCMS\Feeds\Helpers\FeedsHelper;
 
@@ -11,10 +13,15 @@ class FeedController extends AbstractController
     public function render(
         FeedsEntity $feedsEntity,
         FeedsHelper $feedsHelper,
+        Languages   $languages,
+        LanguagesEntity $languagesEntity,
         $url
     ) {
+        $languagesList   = $languagesEntity->mappedBy('id')->find();
+        $currentLanguage = $languagesList[$languages->getLangId()];
+
         $feed = $feedsEntity->findOne(['url' => $url]);
-        if (empty($feed) || (!$feed->enabled && empty($_SESSION['admin']))) {
+        if (empty($feed) || empty($currentLanguage->enabled) || (!$feed->enabled && empty($_SESSION['admin']))) {
             return false;
         }
 
